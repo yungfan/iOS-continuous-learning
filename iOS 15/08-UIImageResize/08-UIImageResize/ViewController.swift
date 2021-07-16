7a9fd421fe1a9045f75d451afcd81344172df854
杨帆 <yangfanvw@qq.com>
2021-07-16 11:38:24 +0800

    UIImage新增调整尺寸API

---
+++ iOS 15/08-UIImageResize/08-UIImageResize/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  08-UIImageResize
+//
+//  Created by 杨帆 on 2021/7/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+        imageView.center = view.center
+        imageView.backgroundColor = .red
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.addSubview(imageView)
+
+        // 1
+        let image = UIImage(named: "sv.png")?.preparingThumbnail(of: CGSize(width: 200, height: 100))
+
+        // 2
+        UIImage(named: "sv.png")?.prepareThumbnail(of: CGSize(width: 200, height: 100)) { image in
+            // 需要回到主线程
+            DispatchQueue.main.async {
+                self.imageView.image = image
+            }
+        }
+
+        // 3
+        Task {
+            await UIImage(named: "sv.png")?.byPreparingThumbnail(ofSize: CGSize(width: 100, height: 100))
+        }
+    }
+}
